0e0b1089a92d2eba9d64fb1aa4b07ed8f4987afd
Alexandre Chau <accounts@chau.moe>
2022-11-21 10:45:43 +0100

    fix: remove legacy iframe resizer

--- src/components/views/read/PlayerView.js
+++ src/components/views/read/PlayerView.js
@@ -1,21 +1,16 @@
-import React, { useState, useEffect, useContext, useRef } from 'react';
+import { Context } from '@graasp/apps-query-client';
+import { styled } from '@mui/material';
 import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { styled } from '@mui/material';
-import { Context } from '@graasp/apps-query-client';
-import Loader from '../../common/Loader';
-import {
-  ADAPT_HEIGHT_TIMEOUT,
-  MAX_INPUT_LENGTH,
-  MAX_ROWS,
-} from '../../../config/settings';
-import { useMutation, MUTATION_KEYS } from '../../../config/queryClient';
-import { hooks } from '../../../config/queryClient';
-import SaveButton from './SaveButton';
-import { inputCypress, inputTextFieldId } from '../../../config/selectors';
 import { ACTION_TYPES } from '../../../config/actionTypes';
 import { APP_DATA_TYPES } from '../../../config/appDataTypes';
+import { hooks, MUTATION_KEYS, useMutation } from '../../../config/queryClient';
+import { inputCypress, inputTextFieldId } from '../../../config/selectors';
+import { MAX_INPUT_LENGTH, MAX_ROWS } from '../../../config/settings';
+import Loader from '../../common/Loader';
+import SaveButton from './SaveButton';
 
 const FormContainer = styled('form')({
   display: 'flex',
@@ -37,7 +32,6 @@ const MainContainer = styled(Grid)(({ theme }) => ({
 const PlayerView = () => {
   const { t } = useTranslation();
   const [text, setText] = useState('');
-  const [height, setHeight] = useState(0);
   const [inputResource, setInputResource] = useState(null);
   const [feedbackResource, setFeedbackResource] = useState(null);
   const rootRef = useRef();
@@ -77,35 +71,6 @@ const PlayerView = () => {
     }
   }, [inputResource]);
 
-  const adaptHeight = () => {
-    // set timeout to leave time for the height to be set
-    setTimeout(() => {
-      // adapt height when not in standalone (so when in an iframe)
-      if (!context?.get('standalone')) {
-        // get height from the root element and add a small margin
-        const clientRect = rootRef?.current?.getBoundingClientRect();
-        if (window.frameElement && clientRect) {
-          const newHeight = clientRect.height;
-          if (height !== newHeight) {
-            setHeight(newHeight);
-            window.frameElement.style['min-height'] = `${newHeight}px`;
-            window.frameElement.style.overflowY = 'hidden';
-            window.frameElement.scrolling = 'no';
-            window.frameElement.style.height = `${newHeight}px`;
-          }
-        }
-      }
-    }, ADAPT_HEIGHT_TIMEOUT);
-  };
-
-  useEffect(
-    () => {
-      adaptHeight();
-    },
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [inputResource]
-  );
-
   if (!context?.get('standalone') && isAppDataLoading) {
     return <Loader />;
   }
@@ -113,7 +78,6 @@ const PlayerView = () => {
   const handleChangeText = ({ target }) => {
     const { value } = target;
     setText(value);
-    adaptHeight();
   };
 
   const handleClickSaveText = () => {
