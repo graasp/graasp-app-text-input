614bc5edbe1d3081fd4fe4f6f2120beb3c795af3
pyphilia <pyphilia@gmail.com>
2023-06-07 16:20:55 +0200

    refactor: upgrade cypress action, enable back analyzer tests

--- cypress/e2e/AnalyzerView.cy.ts
+++ cypress/e2e/AnalyzerView.cy.ts
@@ -1,50 +1,48 @@
-// import { Context, PermissionLevel } from '@graasp/sdk';
-// import {
-//   appTitleCypress,
-//   dataCyWrapper,
-//   logoCypress,
-//   refreshButtonCypress,
-//   wordCloudId,
-// } from '../../src/config/selectors';
-// import { MOCK_APP_DATA, MOCK_FEEDBACK } from '../fixtures/appData';
+import { Context, PermissionLevel } from '@graasp/sdk';
+import {
+  appTitleCypress,
+  dataCyWrapper,
+  logoCypress,
+  refreshButtonCypress,
+  wordCloudId,
+} from '../../src/config/selectors';
+import { MOCK_APP_DATA, MOCK_FEEDBACK } from '../fixtures/appData';
 
-// bug: disabled because it fails in ci
+describe('<AnalyzerView />', () => {
+  describe('Dashboard', () => {
+    it('Display no data', () => {
+      cy.setUpApi({
+        appContext: {
+          permission: PermissionLevel.Admin,
+          context: Context.Analytics,
+        },
+      });
+      cy.visit('/');
 
-// describe('<AnalyzerView />', () => {
-//   describe('Dashboard', () => {
-//     it('Display no data', () => {
-//       cy.setUpApi({
-//         appContext: {
-//           permission: PermissionLevel.Admin,
-//           context: Context.Analytics,
-//         },
-//       });
-//       cy.visit('/');
+      // visible elements
+      cy.get(dataCyWrapper(logoCypress)).should('be.visible');
+      cy.get(dataCyWrapper(appTitleCypress)).should('be.visible');
+      cy.get(dataCyWrapper(refreshButtonCypress)).should('be.visible');
 
-//       // visible elements
-//       cy.get(dataCyWrapper(logoCypress)).should('be.visible');
-//       cy.get(dataCyWrapper(appTitleCypress)).should('be.visible');
-//       cy.get(dataCyWrapper(refreshButtonCypress)).should('be.visible');
+      cy.get(`#${wordCloudId}`).should('not.exist');
+    });
 
-//       cy.get(`#${wordCloudId}`).should('not.exist');
-//     });
+    it('Display data', () => {
+      cy.setUpApi({
+        database: { appData: [MOCK_APP_DATA, MOCK_FEEDBACK] },
+        appContext: {
+          permission: PermissionLevel.Admin,
+          context: Context.Analytics,
+        },
+      });
+      cy.visit('/');
 
-//     it('Display data', () => {
-//       cy.setUpApi({
-//         database: { appData: [MOCK_APP_DATA, MOCK_FEEDBACK] },
-//         appContext: {
-//           permission: PermissionLevel.Admin,
-//           context: Context.Analytics,
-//         },
-//       });
-//       cy.visit('/');
+      // visible elements
+      cy.get(dataCyWrapper(logoCypress)).should('be.visible');
+      cy.get(dataCyWrapper(appTitleCypress)).should('be.visible');
+      cy.get(dataCyWrapper(refreshButtonCypress)).should('be.visible');
 
-//       // visible elements
-//       cy.get(dataCyWrapper(logoCypress)).should('be.visible');
-//       cy.get(dataCyWrapper(appTitleCypress)).should('be.visible');
-//       cy.get(dataCyWrapper(refreshButtonCypress)).should('be.visible');
-
-//       cy.get(`#${wordCloudId} text`).should('contain', 'text');
-//     });
-//   });
-// });
+      cy.get(`#${wordCloudId} text`).should('contain', 'text');
+    });
+  });
+});
