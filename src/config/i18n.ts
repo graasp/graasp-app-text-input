b7f8ea98ea7fe652925d05c011689542bac4b431
spaenleh <spaenleh@gmail.com>
2024-09-06 14:28:21 +0200

    fix: add languages other than french and english

--- src/config/i18n.ts
+++ src/config/i18n.ts
@@ -1,12 +1,20 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import ar from '../langs/ar.json';
+import de from '../langs/de.json';
 import en from '../langs/en.json';
+import es from '../langs/es.json';
 import fr from '../langs/fr.json';
+import it from '../langs/it.json';
 
 i18n.use(initReactI18next).init({
   resources: {
     en,
     fr,
+    de,
+    it,
+    ar,
+    es,
   },
   fallbackLng: 'en',
   // debug only when not in production
