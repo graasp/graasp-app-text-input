a6b3fe8277978ee45dbb1181052dc98190370e9a
Basile Spaenlehauer <spaenleh@gmail.com>
2026-04-09 11:28:41 +0200

    fix: update dependencies (#203)
    * fix: update dependencies
    
    * fix: update
    
    * fix: update dependencies
    
    * fix: update deps
    
    * fix: remove artifact
    
    * fix: upgrade yarn and packages
    
    * fix: update deps
    
    fix: test fixtures
    
    fix: update deps
    
    fix: remove artifact
    
    fix: remove artifact
    
    fix: upgrade yarn and packages
    
    * fix: name of the coverage task

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -12,7 +12,7 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
-import setupCoverage from '@cypress/code-coverage/task';
+import setupCoverage from '@cypress/code-coverage/task.js';
 
 const config: Cypress.PluginConfig = (on, config) => {
   // `on` is used to hook into various events Cypress emits
